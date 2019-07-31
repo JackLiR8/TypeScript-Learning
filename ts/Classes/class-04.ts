0c112e5bdbc9195a23da7de397912e15416d7dd9
李佳珂 <jiakeli1226@163.com>
2019-07-31 10:15:05 +0800

    类的高级技巧

--- ts/Classes/class-04.ts
+++ ts/Classes/class-04.ts
@@ -1,4 +1,8 @@
 /* 
+    Advanced Techniques
+        1. 构造函数
+        2. 类当接口
+        
     ................................ 高级技巧 Advanced Techniques ...........................................
 
     1. 构造函数 constructor functions
@@ -16,21 +20,52 @@
         }
     }
 
-    let greeter: Greeter;
+    let greeter: Greeter;       // Greeter类的实例的类型是 Greeter
     greeter = new Greeter('Ts');
     console.log(greeter.greet())
 
+    // 另一个例子
+    class Greet {
+        static standardGreeting = "Hello,there";
+        greeting: string = '';
+        greet() {
+            if (this.greeting) {
+                return `Hello, ${this.greeting}`
+            } else {
+                return Greet.standardGreeting
+            }
+        }
+    }
 
+    let greeter1: Greet;
+    greeter1 = new Greet();
+    console.log(greeter1.greet());      // Hello,there
 
+    let greeterMaker: typeof Greet = Greet;
+    greeterMaker.standardGreeting = 'Hey, I am maker '
 
+    let greeter2 = new greeterMaker();
+    console.log(greeter2.greet())       // Hey, I am maker 
 
+    /* 
+        greeterMaker 变量保存了类的构造函数；typeof Greet 可以取到 Greet 类的类型，而不是实例的类型；
+        这个类型包含了类的所有静态成员和构造函数。 
+    */
 
+/*  2. 把类当接口使用
 
+            类定义会创建两个东西：类的实例类型和一个构造函数。 因为类可以创建出类型，
+        所以你能够在允许使用接口的地方使用类。
+*/
+    class Point {
+        x: number = 0;
+        y: number = 0;
+    }
 
+    interface Point3d extends Point {
+        z: number;
+    }
 
-
-
-
-
-
+    const point3d: Point3d = {x: 1, y: 2, z: 3}
+    console.log(point3d)
 })()
